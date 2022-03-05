2360c2d100b0e7a43fa9fc161b7253dae937e93f
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-03-05 19:08:49 +0530

    item controller validation aded

--- myprojects/SPA with jQuery/controller/CustomerController.js
+++ myprojects/SPA with jQuery/controller/CustomerController.js
@@ -1,12 +1,224 @@
 
 // CRUD Operations
+$("#btnSave").click(function(){
+    saveCustomer();
+    clearAll();
+    loadAllCustomers();
+}); 
+
+$("#button-addon2").click(function () {
+    var searchID = $("#txtSearchCusID").val();
+
+    var response = searchCustomer(searchID);
+    if (response) {
+        $("#txtCusID").val(response.id);
+        $("#txtCusName").val(response.name);
+        $("#txtCusAddress").val(response.address);
+        $("#txtCusSalary").val(response.salary);
+    }else{
+        clearAll();
+        alert("No Such a Customer");
+    }
+});
+
+function loadAllCustomers() {
+    $("#customerTB").empty();
+    for (var i of customerDB) {
+        let row = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.salary}</td></tr>`;
+        $("#customerTB").append(row);
+    }
+}
+
+
+
 function saveCustomer(){
+let customerID = $("#txtCusID").val();
+    let customerName = $("#txtCusName").val();
+    let customerAddress = $("#txtCusAddress").val();
+    let customerSalary = $("#txtCusSalary").val();
 
+    //create Object
+    var customerObject = {
+        id: customerID,
+        name: customerName,
+        address: customerAddress,
+        salary: customerSalary
+    };
 
+    customerDB.push(customerObject);
+}
+
+function searchCustomer(id) {
+    for (let i = 0; i < customerDB.length; i++) {
+        if (customerDB[i].id == id) {
+            return customerDB[i];
+        }
+    }
+}
+
+function deleteCustomer(){
+   
+}
+
+function updateCustomer(){
+    
 }
 
 
 
 
 
+
 // Validations
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+
+$('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').on('keydown', function (eventOb) {
+    if (eventOb.key == "Tab") {
+        eventOb.preventDefault(); // stop execution of the button
+    }
+});
+
+$('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').on('blur', function () {
+    formValid();
+});
+
+//focusing events
+$("#txtCusID").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+
+    if (eventOb.key == "Control") {
+        var typedCustomerID = $("#txtCusID").val();
+        var srcCustomer = searchCustomerFromID(typedCustomerID);
+        $("#txtCusID").val(srcCustomer.getCustomerID());
+        $("#txtCusName").val(srcCustomer.getCustomerName());
+        $("#txtCusAddress").val(srcCustomer.getCustomerAddress());
+        $("#txtCusSalary").val(srcCustomer.getCustomerSalary());
+    }
+
+
+});
+
+$("#txtCusName").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+
+$("#txtCusAddress").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+
+$("#txtCusSalary").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+// focusing events end
+$("#btnSave").attr('disabled', true);
+
+function clearAll() {
+    $('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').val("");
+    $('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').css('border', '2px solid #ced4da');
+    $('#txtCusID').focus();
+    $("#btnSave").attr('disabled', true);
+    loadAllCustomers();
+    $("#lblcusid,#lblcusname,#lblcusaddress,#lblcussalary").text("");
+}
+
+function formValid() {
+    var cusID = $("#txtCusID").val();
+    $("#txtCusID").css('border', '2px solid green');
+    $("#lblcusid").text("");
+    if (cusIDRegEx.test(cusID)) {
+        var cusName = $("#txtCusName").val();
+        if (cusNameRegEx.test(cusName)) {
+            $("#txtCusName").css('border', '2px solid green');
+            $("#lblcusname").text("");
+            var cusAddress = $("#txtCusAddress").val();
+            if (cusAddressRegEx.test(cusAddress)) {
+                var cusSalary = $("#txtCusSalary").val();
+                var resp = cusSalaryRegEx.test(cusSalary);
+                $("#txtCusAddress").css('border', '2px solid green');
+                $("#lblcusaddress").text("");
+                if (resp) {
+                    $("#txtCusSalary").css('border', '2px solid green');
+                    $("#lblcussalary").text("");
+                    return true;
+                } else {
+                    $("#txtCusSalary").css('border', '2px solid red');
+                    $("#lblcussalary").text("Cus Salary is a required field : Pattern 100.00 or 100");
+                    return false;
+                }
+            } else {
+                $("#txtCusAddress").css('border', '2px solid red');
+                $("#lblcusaddress").text("Cus Name is a required field : Mimum 7");
+                return false;
+            }
+        } else {
+            $("#txtCusName").css('border', '2px solid red');
+            $("#lblcusname").text("Cus Name is a required field : Mimimum 5, Max 20, Spaces Allowed");
+            return false;
+        }
+    } else {
+        $("#txtCusID").css('border', '2px solid red');
+        $("#lblcusid").text("Cus ID is a required field : Pattern C00-000");
+        return false;
+    }
+}
+
+function checkIfValid() {
+    var cusID = $("#txtCusID").val();
+    if (cusIDRegEx.test(cusID)) {
+        $("#txtCusName").focus();
+        var cusName = $("#txtCusName").val();
+        if (cusNameRegEx.test(cusName)) {
+            $("#txtCusAddress").focus();
+            var cusAddress = $("#txtCusAddress").val();
+            if (cusAddressRegEx.test(cusAddress)) {
+                $("#txtCusSalary").focus();
+                var cusSalary = $("#txtCusSalary").val();
+                var resp = cusSalaryRegEx.test(cusSalary);
+                if (resp) {
+                    let res = confirm("Do you really need to add this Customer..?");
+                    if (res) {
+                        saveCustomer();
+                        clearAll();
+                    }
+                } else {
+                    $("#txtCusSalary").focus();
+                }
+            } else {
+                $("#txtCusAddress").focus();
+            }
+        } else {
+            $("#txtCusName").focus();
+        }
+    } else {
+        $("#txtCusID").focus();
+    }
+}
+
+function setButton() {
+    let b = formValid();
+    if (b) {
+        $("#btnSave").attr('disabled', false);
+    } else {
+        $("#btnSave").attr('disabled', true);
+    }
+}
+
+$('#btnSave').click(function () {
+    checkIfValid();
+});
---
+++ myprojects/SPA with jQuery/controller/ItemController.js
@@ -0,0 +1,154 @@
+
+// Validations
+const itemIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const itemNameRegEx = /^[A-z ]{5,20}$/;
+const itemQTYRegEx = /^[0-9/A-z. ,]{7,}$/;
+const unitPriceRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+
+$('#txtItemCode,#txtItemName,#txtItemQTY,#txtUnitPrice').on('keydown', function (eventOb) {
+    if (eventOb.key == "Tab") {
+        eventOb.preventDefault(); 
+    }
+});
+
+$('#txtItemCode,#txtItemName,#txtItemQTY,#txtUnitPrice').on('blur', function () {
+    formValid();
+});
+
+//focusing events
+$("#txtItemCode").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+
+    if (eventOb.key == "Control") {
+        var typedCustomerID = $("#txtItemCode").val();
+        var srcCustomer = searchCustomerFromID(typedCustomerID);
+        $("#txtItemCode").val(srcCustomer.getCustomerID());
+        $("#txtItemName").val(srcCustomer.getCustomerName());
+        $("#txtItemQTY").val(srcCustomer.getCustomerAddress());
+        $("#txtUnitPrice").val(srcCustomer.getCustomerSalary());
+    }
+
+
+});
+
+$("#txtItemName").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+
+$("#txtItemQTY").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+
+$("#txtUnitPrice").on('keyup', function (eventOb) {
+    setButton();
+    if (eventOb.key == "Enter") {
+        checkIfValid();
+    }
+});
+// focusing events end
+$("#btnItemSave").attr('disabled', true);
+
+function clearAll() {
+    $('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').val("");
+    $('#txtCusID,#txtCusName,#txtCusAddress,#txtCusSalary').css('border', '2px solid #ced4da');
+    $('#txtCusID').focus();
+    $("#btnItemSave").attr('disabled', true);
+    loadAllCustomers();
+    $("#txtItemCode,#txtItemName,#txtItemQTY,#txtUnitPrice").text("");
+}
+
+function formValid() {
+    var cusID = $("#txtItemCode").val();
+    $("#txtItemCode").css('border', '2px solid green');
+    $("#txtItemCode").text("");
+    if (cusIDRegEx.test(cusID)) {
+        var cusName = $("#txtItemName").val();
+        if (cusNameRegEx.test(cusName)) {
+            $("#txtItemName").css('border', '2px solid green');
+            $("#txtItemName").text("");
+            var cusAddress = $("#txtItemQTY").val();
+            if (cusAddressRegEx.test(cusAddress)) {
+                var cusSalary = $("#txtUnitPrice").val();
+                var resp = cusSalaryRegEx.test(cusSalary);
+                $("#txtItemQTY").css('border', '2px solid green');
+                $("#txtItemQTY").text("");
+                if (resp) {
+                    $("#txtUnitPrice").css('border', '2px solid green');
+                    $("#txtUnitPrice").text("");
+                    return true;
+                } else {
+                    $("#txtUnitPrice").css('border', '2px solid red');
+                    $("#txtUnitPrice").text("Cus Salary is a required field : Pattern 100.00 or 100");
+                    return false;
+                }
+            } else {
+                $("#txtItemQTY").css('border', '2px solid red');
+                $("#txtItemQTY").text("Cus Name is a required field : Mimum 7");
+                return false;
+            }
+        } else {
+            $("#txtItemName").css('border', '2px solid red');
+            $("#txtItemName").text("Cus Name is a required field : Mimimum 5, Max 20, Spaces Allowed");
+            return false;
+        }
+    } else {
+        $("#txtItemCode").css('border', '2px solid red');
+        $("#txtItemCode").text("Cus ID is a required field : Pattern C00-000");
+        return false;
+    }
+}
+
+function checkIfValid() {
+    var cusID = $("#txtItemCode").val();
+    if (cusIDRegEx.test(cusID)) {
+        $("#txtItemName").focus();
+        var cusName = $("#txtItemName").val();
+        if (cusNameRegEx.test(cusName)) {
+            $("#txtItemQTY").focus();
+            var cusAddress = $("#txtItemQTY").val();
+            if (cusAddressRegEx.test(cusAddress)) {
+                $("#txtUnitPrice").focus();
+                var cusSalary = $("#txtUnitPrice").val();
+                var resp = cusSalaryRegEx.test(cusSalary);
+                if (resp) {
+                    let res = confirm("Do you really need to add this Item..?");
+                    if (res) {
+                        saveItem();
+                        clearAll();
+                    }
+                } else {
+                    $("#txtUnitPrice").focus();
+                }
+            } else {
+                $("#txtItemQTY").focus();
+            }
+        } else {
+            $("#txtItemName").focus();
+        }
+    } else {
+        $("#txtItemCode").focus();
+    }
+}
+
+function setButton() {
+    let b = formValid();
+    if (b) {
+        $("#btnItemSave").attr('disabled', false);
+    } else {
+        $("#btnItemSave").attr('disabled', true);
+    }
+}
+
+$('#btnItemSave').click(function () {
+    checkIfValid();
+});
